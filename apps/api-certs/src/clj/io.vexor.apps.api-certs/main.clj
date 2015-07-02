65e575a1bf30de3aead7e450910fbeeecdebc874
Dmitry Galinsky <dima.exe@gmail.com>
2015-07-02 19:46:33 +0300

    [api-certs] init

---
+++ apps/api-certs/src/clj/io.vexor.apps.api-certs/main.clj
@@ -0,0 +1,7 @@
+(ns io.vexor.apps.api-certs.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]))
+
+(defroutes app
+  (GET "/" [] "<h1>Hello World</h1>")
+  (route/not-found "<h1>Page not found</h1>"))
